e8f1a44a95693ae35853b186dfe3728f863ef1d4
Patrick Morris <pm141088@gmail.com>
2020-06-02 18:18:59 +0100

    Added functionality to add new items to the to do list via add_item.html

--- app.py
+++ app.py
@@ -6,7 +6,26 @@ app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
-    return render_template('index.html', items=session.get_items())
+    return render_template('index.html', get_items=session.get_items())
+
+@app.route('/add_item', methods=['GET', 'POST'])
+def add_item():
+
+    if request.method == 'POST':
+
+        req = request.form
+
+        item = req["title"]
+
+        print(item)
+
+        session.add_item(item)
+
+        return redirect('/')
+
+
+
+    return render_template('add_item.html')
 
 if __name__ == '__main__':
     app.run()
