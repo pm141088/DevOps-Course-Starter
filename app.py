dd7ca73cfbe2d8d8d33f92088168326e6fdf55af
Patrick Morris <pm141088@gmail.com>
2020-07-04 00:48:07 +0100

    Refactored routes in app.py and renamed functions to make it clearer

--- app.py
+++ app.py
@@ -1,57 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
-import session_items as session
+import trello_items
 
 app = Flask(__name__)
-app.config.from_object('flask_config.Config')
 
-# Index.html
 @app.route('/') 
 def index():
-    items = session.get_items()
+    items = trello_items.get_items()
     sorted_items = sorted(items, key=lambda item: item['status'], reverse=True)
     return render_template('index.html', items=sorted_items)
 
-# Add an item
-@app.route('/add_item', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def add_item():
-    if request.method == 'POST':
-        req = request.form
-        title = req['title']
-        status = req['status']
-        new_item = session.add_item(title, status)
-        return redirect(url_for('index'))
-    else:
-        return render_template('add_item.html')
-
-# Read an item by ID
-@app.route('/read_item/<int:id>', methods=['GET'])
-def read_item(id):
-    found_item = session.get_item(id)
-    return render_template('read_item.html', found_item=found_item)
-
-# Delete an item by ID
-@app.route('/read_item/<int:id>/delete', methods=['GET', 'DELETE'])
-def delete_item(id):
-    session.delete_item(id)
+    title = request.form['item_title']
+    description = request.form['item_description']
+    trello_items.add_item(title, description)
     return redirect(url_for('index'))
 
-# Mark an item as 'In Progress'
-@app.route('/read_item/<int:id>/status-in-progress', methods=['PUT', 'POST'])
-def status_in_progress(id):
-    item = session.get_item(id)
-    if item != None:
-        item['status'] = "In Progress"
-        session.save_item(item)
-        return redirect(url_for('index'))
+@app.route('/items/<int:id>', methods=['POST'])
+def complete_item(id):
+    trello_items.complete_item(id)
+    return redirect(url_for('index'))
 
-# Mark an item as 'Completed'
-@app.route('/read_item/<int:id>/status-completed', methods=['PUT', 'POST'])
-def status_completed(id):
-    item = session.get_item(id)
-    if item != None:
-        item['status'] = "Completed"
-        session.save_item(item)
-        return redirect(url_for('index'))
+@app.route('/items/<int:id>', methods=['POST'])
+def in_progress_item(id):
+    trello_items.in_progress_item(id)
+    return redirect(url_for('index'))
+
+@app.route('/items/delete/<int:id>', methods=['POST'])
+def delete_item(id):
+    trello_items.delete_item(id)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
