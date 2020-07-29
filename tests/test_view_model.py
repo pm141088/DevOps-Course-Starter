417be23c049a58415686ed53727a9f9991781135
Patrick Morris <pm141088@gmail.com>
2020-07-30 00:03:08 +0100

    Additional unit tests for test_view_model.py

--- tests/test_view_model.py
+++ tests/test_view_model.py
@@ -1,6 +1,7 @@
 import pytest
 from view_model import ViewModel
 from todo_item_class import ToDoItem, Status
+from datetime import datetime, timedelta
 
 # Fixtures are functions, which will run before each test function to which it is applied.
 # Fixtures are used to feed some data to the tests such input data. 
@@ -10,16 +11,27 @@ from todo_item_class import ToDoItem, Status
 @pytest.fixture
 def items_view_model():
     return ViewModel([
-    ToDoItem(1, "Title 1", Status.TO_DO, "Description 1"),
-    ToDoItem(2, "Title 2", Status.TO_DO, "Description 2"),
-    ToDoItem(3, "Title 3", Status.DOING, "Description 3"),
-    ToDoItem(4, "Title 4", Status.DONE, "Description 4")
+        ToDoItem(1, "Title 1", Status.TO_DO, "Desc #1", datetime.now()),
+        ToDoItem(2, "Title 2", Status.TO_DO, "Desc #2", datetime.now()),
+        ToDoItem(3, "Title 3", Status.DOING, "Desc #3", datetime.now()),
+        ToDoItem(4, "Title 4", Status.DONE, "Desc #4", datetime.now()),
+        ToDoItem(5, "Title 5", Status.DONE, "Desc #5", datetime.now()),
+    ])
+
+@pytest.fixture
+def done_items_view_model():
+    return ViewModel([
+        ToDoItem(6, "Title 1", Status.DONE, "5 days done", datetime.now() - timedelta(days=5)),
+        ToDoItem(7, "Title 2", Status.DONE, "3 hours done", datetime.now() - timedelta(hours=3)),
+        ToDoItem(8, "Title 3", Status.DONE, "Just done", datetime.now()),
+        ToDoItem(9, "Title 4", Status.DONE, "20 days done", datetime.now() - timedelta(days=20)),
+        ToDoItem(10, "Title 5", Status.DONE, "20 hours done", datetime.now() - timedelta(hours=20))
     ])
 
 # A test function can use a fixture by mentioning the fixture name as an input parameter.
 def test_return_all_items(items_view_model):
     all_items = items_view_model.items
-    assert len(all_items) == 4
+    assert len(all_items) == 5
     assert all_items[0].id == 1
 
 def test_return_to_do_items(items_view_model):
@@ -35,5 +47,22 @@ def test_return_doing_items(items_view_model):
 
 def test_return_done_items(items_view_model):
     done_items = items_view_model.done_items
-    assert len(done_items) == 1
+    assert len(done_items) == 2
     assert done_items[0].id == 4
+
+def test_show_all_done_items_true(items_view_model):
+    assert len(items_view_model.show_all_done_items) == 2
+    assert items_view_model.show_all_done_items is True
+
+def test_show_all_done_items_false(done_items_view_model):
+    assert done_items_view_model.show_all_done_items is False
+
+def test_return_recent_done_items(done_items_view_model):
+    recent_done_items = done_items_view_model.recent_done_items
+    assert len(recent_done_items) == 3
+    assert set([item.id for item in recent_done_items]) == set([7, 8, 10])
+
+def test_return_older_done_items(done_items_view_model):
+    older_done_items = done_items_view_model.older_done_items
+    assert len(older_done_items) == 2    
+    assert set([item.id for item in older_done_items]) == set([6, 10])
