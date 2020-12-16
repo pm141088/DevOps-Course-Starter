bb2f642726d4f7e00e6a057fb8859d43d76b9ffa
Patrick Morris <pm141088@gmail.com>
2020-12-16 23:05:41 +0000

    Exercise 8 - Push an image to Heroku and release it

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Get the latest image from Docker Hub (built by your CI pipeline)
-$ docker pull pm141088/todo-app
+docker pull pm141088/todo-app
 # Tag it for Heroku
-$ docker tag pm141088/todo-app registry.heroku.com/pm141088-todo-app/web
+docker tag pm141088/todo-app registry.heroku.com/pm141088-todo-app/web
 # Push it to Heroku registry
-$ docker push registry.heroku.com/pm141088-todo-app/web
-# Release 
-$ heroku container:release web -a pm141088-todo-app
+docker push registry.heroku.com/pm141088-todo-app/web
+# Release the image to your app
+heroku container:release web -a pm141088-todo-app
