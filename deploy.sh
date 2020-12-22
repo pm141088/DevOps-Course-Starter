72066d691663cf19156d91738a7cec68b4cae31e
Patrick Morris <pm141088@gmail.com>
2020-12-22 19:55:28 +0000

    Exercise 8 - install Heroku CLI and parameterise Heroku app name

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Get the latest image from Docker Hub (built by your CI pipeline)
-docker pull pm141088/todo-app:latest
-# Tag it for Heroku
-docker tag pm141088/todo-app registry.heroku.com/pm141088-todo-app/web
 # Push it to Heroku registry
-docker push registry.heroku.com/pm141088-todo-app/web
+docker push registry.heroku.com/$HEROKU_APP/web
 # Release the image to your app
-heroku container:release web --app pm141088-todo-app
+heroku container:release web --app $HEROKU_APP
