5ba0f686409c2decdefba89c27a8c5252dd52291
Patrick Morris <pm141088@gmail.com>
2021-03-24 20:55:32 +0000

    Exercise 10 - Step3: Implementing the callback route

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
+""" Flask-Login 
+Flask-Login provides user session management for Flask. It handles the common tasks of logging in, logging out, and remembering your users’ sessions over extended periods of time.
+https://flask-login.readthedocs.io/en/latest/
+"""
 from flask_login import LoginManager, login_required, login_user, current_user
 from oauthlib.oauth2 import WebApplicationClient
 import requests
@@ -27,27 +31,62 @@ def create_app():
     # User session management setup
     login_manager = LoginManager()
 
+    # This information is obtained upon registration of a new GitHub account
     client_id = os.getenv('CLIENT_ID')
     client_secret = os.getenv('CLIENT_SECRET')
-
     authorization_url = 'https://github.com/login/oauth/authorize'
     access_token_url = 'https://github.com/login/oauth/access_token'
     
     # OAuth 2.0 Client setup
     oauth_client = WebApplicationClient(client_id)
     
+    # Logic to redirect to Github OAuth flow when unauthenticated
     @login_manager.unauthorized_handler
     def unauthenticated():
-        request_uri = oauth_client.prepare_request_uri(
-            authorization_url
-        )
+        request_uri = oauth_client.prepare_request_uri(authorization_url)
         return redirect(request_uri)
 
+    # This callback is used to reload the user object from the user ID stored in the session.
     @login_manager.user_loader
     def load_user(user_id):
         return User(user_id)
 
+    # Once the actual application object has been created, you can configure it for login with: 
     login_manager.init_app(app)
+
+    # Implement the callback route
+    @app.route("/login/callback", methods=["GET"])
+    def callback():
+        token_url, token_headers, token_body = oauth_client.prepare_token_request(
+            token_url="https://github.com/login/oauth/access_token",
+            authorization_response=request.url,
+            client_secret=client_secret
+        )
+        token_response = requests.post(
+            token_url,
+            headers=token_headers,
+            data=token_body
+        )
+        oauth_client.parse_request_body_response(
+            token_response.content.decode()
+        )
+
+        # Add token to the request uri, body or authorization header.
+        user_info_request_url, user_info_request_headers, user_info_request_body = oauth_client.add_token(
+            uri='https://api.github.com/user',
+        )
+        user_info_response = requests.get(
+            user_info_request_url,
+            data=user_info_request_body,
+            headers=user_info_request_headers
+        )
+
+        github_username = user_info_response.json()['login']
+        user = User(github_username)
+        login_success = login_user(user)
+
+        if login_success:
+            return redirect(url_for('index'))
 
     @app.route('/') 
     @login_required
